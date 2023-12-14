11b3b1b880de262cd6c95cde0604a56231a6844c
mahakamal-e <mahakamal09@gmail.com>
2023-12-14 13:51:17 -0800

    make edition on pchar function

--- monty_opcodes_3.c
+++ monty_opcodes_3.c
@@ -9,22 +9,22 @@
 void op_pchar(stack_t **stack_head, unsigned int ln_)
 {
 	int value;
-	stack_t *temp;
+	/*stack_t *temp;*/
 
 	if (*stack_head == NULL)
 	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", ln_);
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", ln_);
 		exit(EXIT_FAILURE);
 	}
 	value = (*stack_head)->n;
 	if (value < 0 || value > 127)
 	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", ln_);
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", ln_);
 		exit(EXIT_FAILURE);
 	}
-	printf("%c\n", (char)value);
+	printf("%c\n", value);
 
-	temp = *stack_head;
-	*stack_head = (*stack_head)->next;
-	free(temp);
+	/*temp = *stack_head;*/
+	/**stack_head = (*stack_head)->next;*/
+	/*free(temp);*/
 }
